17639de7997705d96eead02043cf1d0ed722c9f3
Giampaolo Cimino <gcimino@gmail.com>
2025-01-20 20:58:54 +0100

    added cli main file

---
+++ src/aisdecoder/cli/main.py
@@ -0,0 +1,48 @@
+import sys
+from pathlib import Path
+
+from aisdecoder.message_factory import AllMessagesFactory
+from aisdecoder.vdm_sentence_structure import EndsWithEpochTime
+from aisdecoder.message_iterator import MessageIterator
+from aisdecoder.writers.density_map import DensityMap
+from aisdecoder.writers.writer_csv import WriterCSV
+from aisdecoder.filters.filter_bbox import FilterBBox
+from aisdecoder.basictypes.basic_types import Rectangle
+
+def file_to_csv(fn):
+    with open(fn) as f:
+        it = MessageIterator(
+            f,
+            sentence_structure = EndsWithEpochTime(),
+            message_factory = AllMessagesFactory()
+        )        
+        for msg in it:
+            print(msg.as_csv())
+
+def file_to_netcdf(fn):
+    with open(fn) as f:
+        it = MessageIterator(
+            f,
+            sentence_structure = EndsWithEpochTime(),
+            message_factory = AllMessagesFactory()
+        )
+        map = DensityMap(
+            map_boundaries=Rectangle(7, 41, 12, 45.5),
+            output_file="density_map.png", 
+            filters=[FilterBBox(Rectangle(7, 41, 12, 45.5))]
+        )
+        csv = WriterCSV(Path("kine.csv"))
+        for msg in it:
+            map.add_message(msg)
+            msg.write(csv)
+        map.generate_density_map()
+        csv.close()
+
+if __name__ == "__main__":
+    #pprint(timeit.repeat(plain_aislib, number=1, repeat=1))
+    print("------------------")
+    #[9.004973700000846, 9.574930900000254, 9.082180499999595]
+    
+    filepath = sys.argv[1]  #-extract
+    file_to_netcdf(filepath)
+    #pprint(timeit.repeat(parse_file, number=1, repeat=1))
